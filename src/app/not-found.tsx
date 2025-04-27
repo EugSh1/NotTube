bc14535b97573727a5190bd2dfa7ef1046282a98
EugSh <127679450+EugSh1@users.noreply.github.com>
2025-04-27 08:38:23 +0300

    feat: add title to error page, create Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,14 +2,14 @@ import { formatPageTitle } from "@/lib/formatPageTitle";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: formatPageTitle("Error")
+    title: formatPageTitle("Not Found")
 };
 
-export default function ErrorPage() {
+export default function NotFound() {
     return (
         <div className="w-full h-screen flex flex-col gap-1.5 justify-center items-center">
-            <h1 className="text-2xl font-semibold">Something went wrong</h1>
-            <p className="font-medium">Please try again later.</p>
+            <h1 className="text-2xl font-semibold">Not Found</h1>
+            <p className="font-medium">This page could not be found.</p>
         </div>
     );
 }
